072bdaba1dfbeb3aa812df482b8d9d58708314a8
Garrett Faucher <44652299+GarrettFaucher@users.noreply.github.com>
2025-03-29 13:59:18 -0400

    Macros defined

--- config/keypos_def/keypos_42_keys.h
+++ config/keypos_def/keypos_42_keys.h
@@ -84,6 +84,10 @@
               #define RH0 39  // right thumb keys
               #define RH1 40
               #define RH2 41
+
+              #define KEYS_L LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5  // left-hand keys
+              #define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
+              #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                                  // thumb keys
               
               #define LAYER_FROM60( \
                 k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
