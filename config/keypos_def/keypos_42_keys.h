9bf238a9d8852aaca5d019315aceb6b1e560f19d
Garrett Faucher <garrettfaucher@gmail.com>
2025-06-13 17:52:56 -0400

    New keymap

--- config/keypos_def/keypos_42_keys.h
+++ config/keypos_def/keypos_42_keys.h
@@ -89,14 +89,14 @@
               #define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
               #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                                  // thumb keys
               
-              #define LAYER_FROM60( \
+              #define LAYER_FROM64( \
                 k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
                 k12, k13, k14, k15, k16, k17,           k18, k19, k20, k21, k22, k23, \
                 k24, k25, k26, k27, k28, k29,           k30, k31, k32, k33, k34, k35, \
                 k36, k37, k38, k39, k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, \
-                          k50, k51, k52, k53, k54, k55, k56, k57, k58, k59            \
+                k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, k60, k61, k62, k63  \
               ) \
                 k12  k13  k14  k15  k16  k17            k18  k19  k20  k21  k22  k23  \
                 k24  k25  k26  k27  k28  k29            k30  k31  k32  k33  k34  k35  \
                 k36  k37  k38  k39  k40  k41            k44  k45  k46  k47  k48  k49  \
-                                    k52  k53  k54  k55  k56  k57
+                                    k54  k55  k56  k57  k58  k59
