c5c101b40ebd3091f7887c2576032e0e5fd7f17a
Garrett Faucher <44652299+GarrettFaucher@users.noreply.github.com>
2025-06-13 22:39:20 -0400

    Add keyball61 (#30)
    * Init Keyball61
    
    * No xyz for now
    
    * Keymap changes
    
    * New keymap
    
    * Fixes
    
    * Add config
    
    * Mouse bind
    
    * Pointing for all
    
    * Bind mouse layer

--- config/keypos_def/keypos_61_keys.h
+++ config/keypos_def/keypos_61_keys.h
@@ -26,15 +26,15 @@
 
 /*   
 
-/*                                      60 KEY MATRIX / LAYOUT MAPPING
+/*                                      61 KEY MATRIX / LAYOUT MAPPING
 
   ╭────────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
   │  0   1   2   3   4   5     │      6   7   8   9  10  11 │ │ LN5 LN4 LN3 LN2 LN1 LN0     │     RN0 RN1 RN2 RN3 RN4 RN5 │
   │ 12  13  14  15  16  17     │     18  19  20  21  22  23 │ │ LT5 LT4 LT3 LT2 LT1 LT0     │     RT0 RT1 RT2 RT3 RT4 RT5 │
   │ 24  25  26  27  28  29     │     30  31  32  33  34  35 │ │ LM5 LM4 LM3 LM2 LM1 LM0     │     RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 36  37  38  39  40  41  42 │ 43  44  45  46  47  48  49 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LEC │ REC RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───────╮ 50  51  52  53  54 │ 55  56  57  58  59 ╭───────╯ ╰───────╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───────╯
-          ╰────────────────────┴────────────────────╯                 ╰─────────────────────┴─────────────────────╯         */
+  │ 36  37  38  39  40  41  42 │ 43  44  45  46  47  48  49 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LSN │ RSN RB0 RB1 RB2 RB3 RB4 RB5 │
+  │ 50  51  52  53  54  55  56 │ 57  58              59  60 | | LH6 LH5 LH4 LH3 LH2 LH1 LH0 │ RH0 RH1             RH2 RH3 |
+  ╰────────────────────────────┴────────────────────────────╯ ╰─────────────────────────────┴─────────────────────────────╯         */
 
           #pragma once
 
@@ -87,8 +87,8 @@
           #define LB4 37
           #define LB5 36
           
-          #define LEC 42  // left encoder key
-          #define REC 43  // right encoder key
+          #define LSN 42  // left single
+          #define RSN 43  // right single
           
           #define RB0 44  // right-bottom row
           #define RB1 45
@@ -97,21 +97,22 @@
           #define RB4 48
           #define RB5 49
           
-          #define LH0 54  // left thumb keys
-          #define LH1 53
-          #define LH2 52
-          #define LH3 51
-          #define LH4 50
+          #define LH0 56  // left thumb keys
+          #define LH1 55
+          #define LH2 54
+          #define LH3 53
+          #define LH4 52
+          #define LH5 51
+          #define LH6 50
           
-          #define RH0 55  // right thumb keys
-          #define RH1 56
-          #define RH2 57
-          #define RH3 58
-          #define RH4 59
+          #define RH0 57  // right thumb keys
+          #define RH1 58
+          #define RH2 59
+          #define RH3 60
 
-          #define KEYS_L LN0 LN1 LN2 LN3 LN4 LN5 LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5  // left-hand keys
-          #define KEYS_R RN0 RN1 RN2 RN3 RN4 RN5 RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
-          #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                                                          // thumb keys
+          #define KEYS_L LN0 LN1 LN2 LN3 LN4 LN5 LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5 LH3 LH4 LH5 LH6  // left-hand keys
+          #define KEYS_R RN0 RN1 RN2 RN3         RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5 RH2 RH3          // right-hand keys
+          #define THUMBS LH2 LH1 LH0 RH0 RH1                                                                                              // thumb keys
 
           #define LAYER_FROM64( \
             k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
@@ -124,4 +125,4 @@
             k12  k13  k14  k15  k16  k17            k18  k19  k20  k21  k22  k23  \
             k24  k25  k26  k27  k28  k29            k30  k31  k32  k33  k34  k35  \
             k36  k37  k38  k39  k40  k41  k42  k43  k44  k45  k46  k47  k48  k49  \
-                      k50  k51  k54  k55  k56  k57  k58  k59  k60  k61
+            k50  k51  k52  k53  k54  k55  k56  k57  k58                 k59  k63
