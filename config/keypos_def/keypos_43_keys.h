cdded4030c8cbe7049de928a9c580271e1aa4241
GarrettFaucher <44652299+GarrettFaucher@users.noreply.github.com>
2026-03-14 17:50:02 -0400

    Trying cleanup

--- config/keypos_def/keypos_43_keys.h
+++ config/keypos_def/keypos_43_keys.h
@@ -4,17 +4,17 @@
  * MIT License
  *
  * Copyright (c) 2023 Robert U
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,9 +24,7 @@
  * SOFTWARE.
  */
 
-/*   
-
-/*           43 KEY MATRIX / LAYOUT MAPPING
+/*                              43 KEY MATRIX / LAYOUT MAPPING
 
   ╭────────────────────────┬────────────────────────╮
   │  0   1   2   3   4     │      5   6   7   8   9 │
@@ -41,72 +39,72 @@
   | LH5 LH4 LH3 LH2 LH1 LH0 │ RH0 RH1             RH2 |
   ╰─────────────────────────┴─────────────────────────╯  */
 
-          #pragma once
-          
-          #define LT0 4  // left-top row
-          #define LT1 3
-          #define LT2 2
-          #define LT3 1
-          #define LT4 0
-          
-          #define RT0 5  // right-top row
-          #define RT1 6
-          #define RT2 7
-          #define RT3 8
-          #define RT4 9
-          
-          #define LM0 14  // left-middle row
-          #define LM1 13
-          #define LM2 12
-          #define LM3 11
-          #define LM4 10
-          #define LM5 15
-          
-          #define RM0 17  // right-middle row
-          #define RM1 18
-          #define RM2 19
-          #define RM3 20
-          #define RM4 21
-          #define RM5 16
-          
-          #define LB0 26  // left-bottom row
-          #define LB1 25
-          #define LB2 24
-          #define LB3 23
-          #define LB4 22
-          
-          #define LSN 27  // left single
-          #define RSN 28  // right single
-          
-          #define RB0 29  // right-bottom row
-          #define RB1 30
-          #define RB2 31
-          #define RB3 32
-          #define RB4 33
-          
-          #define LH0 39  // left thumb keys
-          #define LH1 38
-          #define LH2 37
-          #define LH3 36
-          #define LH4 35
-          #define LH5 34
-          
-          #define RH0 40  // right thumb keys
-          #define RH1 41
-          #define RH2 42
+#pragma once
 
-          #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LH3 LH4 LH5   // left-hand keys
-          #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RH2 RH3       // right-hand keys
-          #define THUMBS LH2 LH1 LH0 RH0 RH1                                                           // thumb keys
+#define LT0 4  // left-top row
+#define LT1 3
+#define LT2 2
+#define LT3 1
+#define LT4 0
 
-              #define LAYER_FROM64( \
-                k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
-                k12, k13, k14, k15, k16, k17,           k18, k19, k20, k21, k22, k23, \
-                k24, k25, k26, k27, k28, k29,           k30, k31, k32, k33, k34, k35, \
-                k36, k37, k38, k39, k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, \
-                k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, k60, k61, k62, k63  \
-              ) \
-                k13  k14  k15  k16  k17            k18  k19  k20  k21  k22 \
-                k25  k26  k27  k28  k29  k24  k35  k30  k31  k32  k33  k34 \
-                k37  k38  k39  k40  k41  k42  k43  k44  k45  k46  k47  k48 \
-                k51  k52  k53  k54  k55  k56  k57  k58                 k59
+#define RT0 5  // right-top row
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+#define LM0 14  // left-middle row
+#define LM1 13
+#define LM2 12
+#define LM3 11
+#define LM4 10
+#define LM5 15
+
+#define RM0 17  // right-middle row
+#define RM1 18
+#define RM2 19
+#define RM3 20
+#define RM4 21
+#define RM5 16
+
+#define LB0 26  // left-bottom row
+#define LB1 25
+#define LB2 24
+#define LB3 23
+#define LB4 22
+
+#define LSN 27  // left single
+#define RSN 28  // right single
+
+#define RB0 29  // right-bottom row
+#define RB1 30
+#define RB2 31
+#define RB3 32
+#define RB4 33
+
+#define LH0 39  // left thumb keys
+#define LH1 38
+#define LH2 37
+#define LH3 36
+#define LH4 35
+#define LH5 34
+
+#define RH0 40  // right thumb keys
+#define RH1 41
+#define RH2 42
+
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LH3 LH4 LH5  // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RH2               // right-hand keys
+#define THUMBS LH2 LH1 LH0 RH0 RH1                                                                // thumb keys
+
+#define LAYER_FROM64( \
+    k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
+    k12, k13, k14, k15, k16, k17,           k18, k19, k20, k21, k22, k23, \
+    k24, k25, k26, k27, k28, k29,           k30, k31, k32, k33, k34, k35, \
+    k36, k37, k38, k39, k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, \
+    k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, k60, k61, k62, k63  \
+) \
+    k13  k14  k15  k16  k17            k18  k19  k20  k21  k22 \
+    k25  k26  k27  k28  k29  k24  k35  k30  k31  k32  k33  k34 \
+    k37  k38  k39  k40  k41  k42  k43  k44  k45  k46  k47  k48 \
+    k51  k52  k53  k54  k55  k56  k57  k58                 k59
